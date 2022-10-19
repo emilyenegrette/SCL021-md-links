68a04399f499975a441a83ed4cd2d4d52a2d2cc2
emilyenegrette <emilyenegrette@gmail.com>
2022-10-19 16:50:44 -0300

    last changes

--- cli.js
+++ cli.js
@@ -39,23 +39,3 @@ else {
 
 }
 
-// revisar referencia que paso Jackie para el process.argv jiji
-/* const {mdLinks, absoluteLink} = require("./index.js")
-const input = [process.argv[3], process.argv[4]]
-let options = "";
-
-if(input[0] && input[1] === undefined) {
-    options = input[0]
-} else if (input[0] && input[1]){
-    options = input [0] + ' ' + input[1]
-} else {
-    options = "--stats"
-}
-
-mdLinks(absoluteLink, options)
-.then(out => {
-    console.log(out)
-})
-.catch(error => {
-    console.log(error)
-}) */
